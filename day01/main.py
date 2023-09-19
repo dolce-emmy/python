1499be1fb70919c2e9c4a552aa26c54274f2a0e6
Eman Yassin <eman.yassin83@gmail.com>
2023-09-19 10:55:21 +0200

    updated changes

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,9 @@
+# print is a function that takes an input and prints it to the console.
+
 print("Hello World!")
 
 print("Hello World!\nHello World!\nHello World!")
+
+print("Hello" + " " + "Eman")
+
+print(input("What is your name? "))
---
+++ day01/main2.py
@@ -0,0 +1,12 @@
+#varaibles
+
+name = "John"
+print(name)
+
+name = "Jack"
+print(name)
+
+name = input("What is your name? ")
+length = len(name)
+
+print(length)
